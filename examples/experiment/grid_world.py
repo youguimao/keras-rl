d59d5ecdb5807a980b94adc2dd3315b36db830c7
youguimao <59811828+youguimao@users.noreply.github.com>
2020-01-14 14:26:07 +0800

    complete the policy iteration for grid world

--- examples/experiment/grid_world.py
+++ examples/experiment/grid_world.py
@@ -3,7 +3,7 @@ import numpy as np
 WORLD_HEIGHT = 4
 WORLD_WIDTH = 4
 
-GOAL = [0, 0]
+GOAL = [[0, 0], [3, 3]]
 
 ACTION_UP = 0
 ACTION_DOWN = 1
@@ -24,18 +24,17 @@ def step(state, action):
         next_state = [i, min(j + 1, WORLD_WIDTH -1)]
     reward = -1
 
-    if state == GOAL:
+    if state in GOAL:
         next_state = state
         reward = 0
-
     return next_state, reward
 
 
 def compute_state_value(discount = 1):
     new_state_value = np.zeros((WORLD_HEIGHT, WORLD_WIDTH))
-    while True
-        state_value = np.deepcopy(new_state_value)
-        old_state_value = np.deepcopy(state_value)
+    while True:
+        state_value = np.copy(new_state_value)
+        old_state_value = np.copy(state_value)
 
         for i in range(WORLD_HEIGHT):
             for j in range(WORLD_WIDTH):
@@ -43,10 +42,48 @@ def compute_state_value(discount = 1):
                 for action in ACTIONS:
                     next_state, reward = step([i, j], action)
                     value += ACTION_PROB * (reward + discount * state_value[next_state[0], next_state[1]])
-                    next_state_value[i, j] = value
+                    new_state_value[i, j] = value
 
         max_delta = np.max(abs(old_state_value - new_state_value))
         if max_delta < 1e-4:
             break
 
+    print('state value is:')
+    for row in new_state_value:
+        print(row)
     return new_state_value
+
+
+def print_optimal_policy(state_value):
+    optimal_policy = []
+    for i in range(WORLD_HEIGHT):
+        optimal_policy.append([])
+        for j in range(WORLD_WIDTH):
+            if [i, j] in GOAL:
+                optimal_policy[-1].append('G')
+                continue
+
+            action_value = np.zeros((1, 4))
+            for action in ACTIONS:
+                next_state, reward = step([i, j], action)
+                action_value[0, action] = state_value[next_state[0], next_state[1]]
+
+            best_action = np.argmax(action_value)
+
+            if best_action == ACTION_UP:
+                optimal_policy[-1].append('U')
+            elif best_action == ACTION_DOWN:
+                optimal_policy[-1].append('D')
+            elif best_action == ACTION_LEFT:
+                optimal_policy[-1].append('L')
+            elif best_action == ACTION_RIGHT:
+                optimal_policy[-1].append('R')
+
+    for row in optimal_policy:
+        print(row)
+
+
+if __name__ == '__main__':
+    state_value = compute_state_value(1)
+    print('Optimal policy of Policy iteration:')
+    print_optimal_policy(state_value)
